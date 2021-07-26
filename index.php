5b6ee92ca8264548620222c4f1c31e962f2587af
Leonid <alraun_work@mail.ru>
2021-07-26 17:41:37 +0300

    добавил запятые

--- index.php
+++ index.php
@@ -7,35 +7,35 @@ $posts = [
         'type_post' => 'post-quote',
         'content_post' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
         'username_post' => 'Лариса',
-        'avatar_post' => 'userpic-larisa-small.jpg'
+        'avatar_post' => 'userpic-larisa-small.jpg',
     ],
     [
         'title_post' => 'Игра престолов',
         'type_post' => 'post-text',
         'content_post' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
         'username_post' => 'Владик',
-        'avatar_post' => 'userpic.jpg'
+        'avatar_post' => 'userpic.jpg',
     ],
     [
         'title_post' => 'Наконец, обработал фотки!',
         'type_post' => 'post-photo',
         'content_post' => 'rock-medium.jpg',
         'username_post' => 'Виктор',
-        'avatar_post' => 'userpic-mark.jpg'
+        'avatar_post' => 'userpic-mark.jpg',
     ],
     [
         'title_post' => 'Моя мечта',
         'type_post' => 'post-photo',
         'content_post' => 'coast-medium.jpg',
         'username_post' => 'Лариса',
-        'avatar_post' => 'userpic-larisa-small.jpg'
+        'avatar_post' => 'userpic-larisa-small.jpg',
     ],
     [
         'title_post' => 'Лучшие курсы',
         'type_post' => 'post-link',
         'content_post' => 'www.htmlacademy.ru',
         'username_post' => 'Владик',
-        'avatar_post' => 'userpic.jpg'
+        'avatar_post' => 'userpic.jpg',
     ]
 ];
 
