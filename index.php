e4cf167df53d8b847c2c99cd8920f3a1f0215b8d
Leonid <alraun_work@mail.ru>
2021-08-04 14:03:23 +0300

    сделал шаблон, за исключением защиты от xss

--- index.php
+++ index.php
@@ -6,5 +6,7 @@ $pageContent=include_template('main.php', ['posts' => $posts]);
 $layoutContent=include_template('layout.php', [
     'title' => 'readme: популярное',
     'content' => $pageContent,
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
 ]);
 echo ($layoutContent);
