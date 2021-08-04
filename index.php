897b891630bc2b117d5d2afe61079d16d226cbb1
Leonid <alraun_work@mail.ru>
2021-08-04 13:56:43 +0300

    оо, одну ошибку нашел

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once('helpers.php');
 require_once('data.php');
 
-$pageContent=include_template('main.php', ['posts' => '$posts']);
+$pageContent=include_template('main.php', ['posts' => $posts]);
 $layoutContent=include_template('layout.php', [
     'title' => 'readme: популярное',
     'content' => $pageContent,
